409b9b5e93340e1a461c7961aa9f4b3fec011464
Joe Reeve <jreeve36@gmail.com>
2023-11-08 13:08:42 -1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,249 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+	Chart as ChartJS,
+	RadialLinearScale,
+	PointElement,
+	LineElement,
+	Filler,
+	Tooltip,
+	Legend,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
+import {
+	champ2022,
+	champ2021,
+	champ2020,
+	champ2019,
+	champ2016,
+	champ2014,
+	champ2013,
+	champ2011,
+	champ2008,
+	champ2004,
+} from "./data";
+ChartJS.register(
+	RadialLinearScale,
+	PointElement,
+	LineElement,
+	Filler,
+	Tooltip,
+	Legend
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const data = {
+		labels: [
+			"FG%",
+			"3P%",
+			"FT%",
+			"ORB",
+			"DRB",
+			"AST",
+			"STL",
+			"BLK",
+			"TOV",
+			// "PTS",
+		],
+		datasets: [
+			{
+				label: champ2022["Team"],
+				data: [
+					champ2022["FG%"] * 100,
+					champ2022["3P%"] * 100,
+					champ2022["FT%"] * 100,
+					champ2022["ORB"],
+					champ2022["DRB"],
+					champ2022["AST"],
+					champ2022["STL"],
+					champ2022["BLK"],
+					champ2022["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(255, 99, 132, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+				options: {
+					scale: {
+						min: 0,
+						max: 100,
+					},
+				},
+			},
+			{
+				label: champ2021["Team"],
+				data: [
+					champ2021["FG%"] * 100,
+					champ2021["3P%"] * 100,
+					champ2021["FT%"] * 100,
+					champ2021["ORB"],
+					champ2021["DRB"],
+					champ2021["AST"],
+					champ2021["STL"],
+					champ2021["BLK"],
+					champ2021["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(100, 99, 132, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2020["Team"],
+				data: [
+					champ2020["FG%"] * 100,
+					champ2020["3P%"] * 100,
+					champ2020["FT%"] * 100,
+					champ2020["ORB"],
+					champ2020["DRB"],
+					champ2020["AST"],
+					champ2020["STL"],
+					champ2020["BLK"],
+					champ2020["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(255, 99, 0, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2019["Team"],
+				data: [
+					champ2019["FG%"] * 100,
+					champ2019["3P%"] * 100,
+					champ2019["FT%"] * 100,
+					champ2019["ORB"],
+					champ2019["DRB"],
+					champ2019["AST"],
+					champ2019["STL"],
+					champ2019["BLK"],
+					champ2019["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(178, 102, 255, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2016["Team"],
+				data: [
+					champ2016["FG%"] * 100,
+					champ2016["3P%"] * 100,
+					champ2016["FT%"] * 100,
+					champ2016["ORB"],
+					champ2016["DRB"],
+					champ2016["AST"],
+					champ2016["STL"],
+					champ2016["BLK"],
+					champ2016["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(255, 00, 255, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2014["Team"],
+				data: [
+					champ2014["FG%"] * 100,
+					champ2014["3P%"] * 100,
+					champ2014["FT%"] * 100,
+					champ2014["ORB"],
+					champ2014["DRB"],
+					champ2014["AST"],
+					champ2014["STL"],
+					champ2014["BLK"],
+					champ2014["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(0, 255, 255, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2013["Team"],
+				data: [
+					champ2013["FG%"] * 100,
+					champ2013["3P%"] * 100,
+					champ2013["FT%"] * 100,
+					champ2013["ORB"],
+					champ2013["DRB"],
+					champ2013["AST"],
+					champ2013["STL"],
+					champ2013["BLK"],
+					champ2013["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(255, 255, 0, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2011["Team"],
+				data: [
+					champ2011["FG%"] * 100,
+					champ2011["3P%"] * 100,
+					champ2011["FT%"] * 100,
+					champ2011["ORB"],
+					champ2011["DRB"],
+					champ2011["AST"],
+					champ2011["STL"],
+					champ2011["BLK"],
+					champ2011["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(0, 00, 255, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2008["Team"],
+				data: [
+					champ2008["FG%"] * 100,
+					champ2008["3P%"] * 100,
+					champ2008["FT%"] * 100,
+					champ2008["ORB"],
+					champ2008["DRB"],
+					champ2008["AST"],
+					champ2008["STL"],
+					champ2008["BLK"],
+					champ2008["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(0, 255, 0, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+			{
+				label: champ2004["Team"],
+				data: [
+					champ2004["FG%"] * 100,
+					champ2004["3P%"] * 100,
+					champ2004["FT%"] * 100,
+					champ2004["ORB"],
+					champ2004["DRB"],
+					champ2004["AST"],
+					champ2004["STL"],
+					champ2004["BLK"],
+					champ2004["TOV"],
+					// champ2022["PTS"],
+				],
+				backgroundColor: "rgba(255, 0, 0, 0.2)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				borderWidth: 1,
+			},
+		],
+	};
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="chart">
+					<Radar data={data} />;
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,153 @@
+const champ2022 = {
+	Team: "Golden State Warriors*",
+	"FG%": ".469",
+	"3P%": ".364",
+	"2P": "26.2",
+	"FT%": ".769",
+	ORB: "9.8",
+	DRB: "35.7",
+	AST: "27.1",
+	STL: "8.8",
+	BLK: "4.5",
+	TOV: "14.9",
+	PTS: "111.0",
+};
+
+const champ2021 = {
+	Team: "Miluakee Bucks",
+	"FG%": ".487",
+	"3P%": ".389",
+	"FT%": ".760",
+	ORB: "10.3",
+	DRB: "37.8",
+	AST: "25.5",
+	STL: "8.1",
+	BLK: "4.6",
+	TOV: "13.8",
+	PTS: "120.1",
+};
+
+const champ2020 = {
+	Team: "Los Angeles Lakers",
+	"FG%": ".480",
+	"3P%": ".349",
+	"FT%": ".729",
+	ORB: "10.7",
+	DRB: "35.1",
+	AST: "25.4",
+	STL: "8.6",
+	BLK: "6.6",
+	TOV: "15.2",
+	PTS: "113.4",
+};
+
+const champ2019 = {
+	Team: "Toronto Raptors",
+	"FG%": ".474",
+	"3P%": ".366",
+	"FT%": ".804",
+	ORB: "9.6",
+	DRB: "35.6",
+	AST: "25.4",
+	STL: "8.3",
+	BLK: "5.3",
+	TOV: "14.0",
+	PTS: "114.4",
+};
+
+const champ2016 = {
+	Team: "Cleveland Caveliers",
+	"FG%": ".487",
+	"3P%": ".363",
+	"FT%": ".778",
+	ORB: "9.7",
+	DRB: "32.0",
+	AST: "24.9",
+	STL: "7.2",
+	BLK: "4.4",
+	TOV: "13.6",
+	PTS: "112.0",
+};
+
+const champ2014 = {
+	Team: "San Antonio Spurs",
+	"FG%": ".486",
+	"3P%": ".397",
+	"FT%": ".785",
+	ORB: "9.3",
+	DRB: "34.0",
+	AST: "25.2",
+	STL: "7.4",
+	BLK: "5.1",
+	TOV: "14.4",
+	PTS: "105.4",
+};
+
+const champ2013 = {
+	Team: "Miami Heat",
+	"FG%": ".496",
+	"3P%": ".396",
+	"FT%": ".754",
+	ORB: "8.2",
+	DRB: "30.4",
+	AST: "23.0",
+	STL: "8.7",
+	BLK: "5.4",
+	TOV: "13.9",
+	PTS: "102.9",
+};
+
+const champ2011 = {
+	Team: "Dallas Mavericks",
+	"FG%": ".475",
+	"3P%": ".365",
+	"FT%": ".777",
+	ORB: "9.5",
+	DRB: "31.9",
+	AST: "23.8",
+	STL: "6.8",
+	BLK: "4.3",
+	TOV: "14.0",
+	PTS: "100.2",
+};
+
+const champ2008 = {
+	Team: "Boston Celtics",
+	"FG%": ".475",
+	"3P%": ".381",
+	"FT%": ".771",
+	ORB: "10.1",
+	DRB: "31.9",
+	AST: "22.4",
+	STL: "8.5",
+	BLK: "4.6",
+	TOV: "15.2",
+	PTS: "100.5",
+};
+
+const champ2004 = {
+	Team: "Detroit Pistons",
+	"FG%": ".435",
+	"3P%": ".344",
+	"FT%": ".753",
+	ORB: "12.4",
+	DRB: "30.4",
+	AST: "20.8",
+	STL: "8.0",
+	BLK: "7.0",
+	TOV: "15.1",
+	PTS: "90.1",
+};
+
+export {
+	champ2022,
+	champ2021,
+	champ2020,
+	champ2019,
+	champ2016,
+	champ2014,
+	champ2013,
+	champ2011,
+	champ2008,
+	champ2004,
+};
